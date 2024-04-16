84e495784ab5c6cca420f1d98c83ccd822a1f9cc
mararok <5163714+Mararok@users.noreply.github.com>
2024-04-16 22:28:46 +0200

    feat: add HcAppRedisModule

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
 PROJECT = hc-cloud
 
+init_tmp:
+	mkdir -p ./tmp/redis
+	chmod 0777 -R ./tmp
+	chmod 0777 -R ./docker
+	
 up:
 	docker compose -p $(PROJECT) --env-file ./docker/.env up -d --wait --wait-timeout 5
-	sleep 2
-	docker logs hc-cloud-s3
+	@sleep 3
+	docker compose -p hc-cloud logs s3
+	docker compose -p hc-cloud logs redis
 down:
 	docker compose -p $(PROJECT) down -t 2
 
---
+++ test/integration/Infrastructure/Redis/HcAppRedisModule.test.ts
@@ -0,0 +1,45 @@
+/**
+ * @group integration
+ */
+
+import { Test, TestingModule } from '@nestjs/testing';
+import { type Redis } from 'ioredis';
+import { HcAppRedisModule, InjectAppRedis } from '@';
+import { Injectable } from '@nestjs/common';
+import { HcAppConfigModule } from '@hexancore/core';
+
+@Injectable()
+class T {
+  public constructor(@InjectAppRedis() public r: Redis) {}
+}
+
+describe('HcAppRedisModule', () => {
+  let module: TestingModule;
+  let redis: Redis;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [HcAppConfigModule, HcAppRedisModule],
+      providers: [T],
+    }).compile();
+    module.enableShutdownHooks();
+    await module.init();
+    redis = module.get(T).r;
+  }, 10000);
+
+  afterAll(async () => {
+    if (module) {
+      redis = null;
+      await module.close();
+      module = null;
+    }
+  });
+
+  test('connected', async () => {
+    const key = 'app:test';
+    await redis.set(key, 'test');
+    const r = await redis.get(key);
+
+    expect(r).toEqual('test');
+  }, 10000);
+});
